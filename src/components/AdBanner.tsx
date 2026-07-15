5702f7b391ab0fb8145f5d756bf393e6399f8f9e
FUHEKl <brahmi.mouhamedrayen@hotmail.com>
2026-07-15 13:57:06 +0100

    Add Google AdSense publisher ID

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -27,7 +27,7 @@ export function AdBanner({ className = '', dataAdSlot = '1234567890', dataAdForm
       <ins
         className="adsbygoogle"
         style={{ display: 'block', width: '100%', minHeight: '90px', backgroundColor: 'rgba(0,0,0,0.2)' }}
-        data-ad-client="ca-pub-0000000000000000" /* REPLACE WITH YOUR ADSENSE PUBLISHER ID */
+        data-ad-client="ca-pub-9798277493807063" /* REPLACE WITH YOUR ADSENSE PUBLISHER ID */
         data-ad-slot={dataAdSlot} /* REPLACE WITH YOUR AD SLOT ID */
         data-ad-format={dataAdFormat}
         data-full-width-responsive="true"
